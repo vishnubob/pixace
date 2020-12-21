edf388932b5376e5e33cfc64a6db13964d5e2d5f
Giles Hall <giles@polymerase.org>
2020-12-21 17:34:33 -0500

    remove tensorflow memory twiddle

--- imgtr/__init__.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  try:
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-  except RuntimeError as e:
-    print(e)
