7aa9d88e239dfe2b6e555f7b17a88b79ae306dad
Giles Hall <giles@polymerase.org>
2020-12-23 23:35:25 -0500

    absl sys.exit path

--- pixace/__init__.py
+++ pixace/__init__.py
@@ -1,2 +1,27 @@
+import sys
 from . import tokens
-from . main import *
+
+def wrap_command(command, **kw):
+    argv = [sys.argv[0], command]
+    argv += [f"--{key}={val}" for (key, val) in kw.items()]
+    sys.argv = argv
+
+    # absl workaround
+    old_exit = sys.exit
+    def no_exit(val):
+        return
+    sys.exit = no_exit
+    try:
+        from . main import cli
+        cli()
+    finally:
+        sys.exit = old_exit
+
+def train(**kw):
+    wrap_command("train", **kw)
+
+def predict(**kw):
+    wrap_command("predict", **kw)
+
+def download(**kw):
+    wrap_command("download", **kw)
--- pixace/main.py
+++ pixace/main.py
@@ -7,8 +7,6 @@ from . train import train_model
 from . predict import predict_model
 from . zoo import download_model
 
-__all__ = ["cli", "train", "predict", "download"]
-
 def _handle_flags(argv):
     FLAGS.bitdepth = [int(bit) for bit in FLAGS.bitdepth]
     FLAGS.model_dir = os.path.join(FLAGS.model_dir, FLAGS.model_name)
@@ -29,18 +27,3 @@ def cli():
     command = sys.argv[1]
     load_flags(command)
     app.run(runner)
-
-def wrap_command(command, **kw):
-    argv = [sys.argv[0], command]
-    argv += [f"--{key}={val}" for (key, val) in kw.items()]
-    sys.argv = argv
-    return cli()
-
-def train(**kw):
-    return wrap_command("train", **kw)
-
-def predict(**kw):
-    return wrap_command("predict", **kw)
-
-def download(**kw):
-    return wrap_command("download", **kw)
